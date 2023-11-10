05c124a4d4f44beaf7341516b0710dcbff06e5ff
jeason <2055469819@qq.com>
2023-11-10 09:59:15 +0800

    add get_finite() fun

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+
+##' get finite number
+##'
+##'
+##' @title get the finite number
+##' @param x a vector of number
+##' @return vector
+##' @author Erjie Zhao
+##' @export
+get_infinite <- function(x) {
+  x[is.finite(x)]
+}
